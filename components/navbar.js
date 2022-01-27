e4e04207a5376eb286cbf9989087aed64a0727ce
peter_alfie <alfie1104@gmail.com>
2022-01-27 23:00:36 +0900

    support dark theme

--- components/navbar.js
+++ components/navbar.js
@@ -16,6 +16,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -75,6 +76,8 @@ const Navbar = props => {
           </LinkItem>
         </Stack>
         <Box flex={1} align="right">
+          <ThemeToggleButton />
+
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
