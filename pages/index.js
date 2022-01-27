1c0e73d7492c852c371d59695f911b5887d90e50
peter_alfie <alfie1104@gmail.com>
2022-01-27 23:29:46 +0900

    Profile Image and Comment

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Korea!
       </Box>
 
@@ -12,6 +24,23 @@ const Page = () => {
             Peter Alfie
           </Heading>
           <p>Web Fullstack Developer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile-image.png"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
