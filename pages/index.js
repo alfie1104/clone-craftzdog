46054cb2d96e7b774e1e7709332fb1197882a821
peter_alfie <alfie1104@gmail.com>
2022-01-29 05:21:00 +0900

    feat(index.js and works.js and article.js): implement page transitions with Framer Motion

--- pages/index.js
+++ pages/index.js
@@ -9,88 +9,91 @@ import {
   Button
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
 import Section from '../components/section'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Korea!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Peter Alfie
-          </Heading>
-          <p>Web Fullstack Developer</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/profile-image.png"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a full-stack developer based in Korea!
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant={'section-title'}>
-          Work
-        </Heading>
-        <Paragraph>
-          Work Paragraph blah blah called{' '}
-          <NextLink href="/works/inkdrop">
-            <Link>Inkdrop</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Peter Alfie
+            </Heading>
+            <p>Web Fullstack Developer</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/profile-image.png"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant={'section-title'}>
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>19XX</BioYear>
-          Born in Korea
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Learning Next.js and Chakra-ui by cloning craftzdog site
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant={'section-title'}>
-          I Love
-        </Heading>
-        <Paragraph>Music, Blockchain, Investment</Paragraph>
-      </Section>
-    </Container>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant={'section-title'}>
+            Work
+          </Heading>
+          <Paragraph>
+            Work Paragraph blah blah called{' '}
+            <NextLink href="/works/inkdrop">
+              <Link>Inkdrop</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant={'section-title'}>
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>19XX</BioYear>
+            Born in Korea
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Learning Next.js and Chakra-ui by cloning craftzdog site
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant={'section-title'}>
+            I Love
+          </Heading>
+          <Paragraph>Music, Blockchain, Investment</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
 import thumbWork1 from '../public/images/works/inkdrop_eyecatch.png'
 import thumbWork2 from '../public/images/works/walknote_eyecatch.png'
@@ -14,115 +15,117 @@ import thumbWork9 from '../public/images/works/amembo_eyecatch.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="work1Title"
-            title="work1Title"
-            thumbnail={thumbWork1}
-          >
-            Web description blah blah
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="work2Title"
-            title="work2Title"
-            thumbnail={thumbWork2}
-          >
-            Tool description blah blah
-          </WorkGridItem>
-        </Section>
-
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="work3Title"
-            title="work3Title"
-            thumbnail={thumbWork3}
-          >
-            An app the suggests ~~~ based on ~.
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="work4Title"
-            title="work4Title"
-            thumbnail={thumbWork4}
-          >
-            An app the suggests ~~~ based on ~.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-
-      <Section delay={0.2}>
-        <Divider my={6} />
+    <Layout>
+      <Container>
         <Heading as="h3" fontSize={20} mb={4}>
-          Collaborations
+          Works
         </Heading>
-      </Section>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="work1Title"
+              title="work1Title"
+              thumbnail={thumbWork1}
+            >
+              Web description blah blah
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="work2Title"
+              title="work2Title"
+              thumbnail={thumbWork2}
+            >
+              Tool description blah blah
+            </WorkGridItem>
+          </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="work5Title"
-            title="work5Title"
-            thumbnail={thumbWork5}
-          >
-            description on work5
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="work6Title"
-            title="work6Title"
-            thumbnail={thumbWork6}
-          >
-            description on work6
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="work3Title"
+              title="work3Title"
+              thumbnail={thumbWork3}
+            >
+              An app the suggests ~~~ based on ~.
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="work4Title"
+              title="work4Title"
+              thumbnail={thumbWork4}
+            >
+              An app the suggests ~~~ based on ~.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
 
-      <Section delay={0.4}>
-        <Divider my={6} />
-        <Heading as="h3" fontSize={20} mb={4}>
-          Old works
-        </Heading>
-      </Section>
+        <Section delay={0.2}>
+          <Divider my={6} />
+          <Heading as="h3" fontSize={20} mb={4}>
+            Collaborations
+          </Heading>
+        </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="work7Title"
-            title="work7Title"
-            thumbnail={thumbWork7}
-          >
-            description on work7
-          </WorkGridItem>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="work5Title"
+              title="work5Title"
+              thumbnail={thumbWork5}
+            >
+              description on work5
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="work6Title"
+              title="work6Title"
+              thumbnail={thumbWork6}
+            >
+              description on work6
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+
+        <Section delay={0.4}>
+          <Divider my={6} />
+          <Heading as="h3" fontSize={20} mb={4}>
+            Old works
+          </Heading>
         </Section>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="work8Title"
-            title="work8Title"
-            thumbnail={thumbWork8}
-          >
-            description on work8
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.6}>
-          <WorkGridItem
-            id="work9Title"
-            title="work9Title"
-            thumbnail={thumbWork9}
-          >
-            description on work9
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="work7Title"
+              title="work7Title"
+              thumbnail={thumbWork7}
+            >
+              description on work7
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="work8Title"
+              title="work8Title"
+              thumbnail={thumbWork8}
+            >
+              description on work8
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.6}>
+            <WorkGridItem
+              id="work9Title"
+              title="work9Title"
+              thumbnail={thumbWork9}
+            >
+              description on work9
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
